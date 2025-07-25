8f2dc286e8755ed8c1d4b36247d4b302d5642df6
Dan Carlton <danlouis.carlton@gmail.com>
2025-07-24 19:42:04 -0700

    formatted url in api request

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,20 +1,22 @@
 const BadRequestError = require('../errors/BadRequestError')
 const Article = require('../models/Articles')
 
-// const formattedDate = new Date().toISOString().slice(0, 10)
+const toDate = new Date().toISOString().slice(0, 10)
+const fromDate = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+  .toISOString()
+  .slice(0, 10)
 
 // GET /articles
 // get all articles from news api
 exports.getArticles = async (req, res, next) => {
   try {
     const search = req.query.q
-    console.log('search query:', search)
     if (!search) {
       throw new BadRequestError('Search query is required.')
     }
 
     const response =
-      await fetch(`https://newsapi.org/v2/everything?q=${search}&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}
+      await fetch(`https://newsapi.org/v2/everything?q=${search}&from=${fromDate}&to=${toDate}&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}
       `)
 
     const data = await response.json()
