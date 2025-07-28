f1c1a70599915b706501b8d1709ab12939ba915f
Dan Carlton <danlouis.carlton@gmail.com>
2025-07-27 17:19:01 -0700

    updated userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
-const { register, login } = require('../controllers/userController')
+const { register, login, getUser } = require('../controllers/userController')
+const auth = require( '../middleware/auth' )
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/me', auth, getUser)
 
 module.exports = router
